b226b9f8fb32754ad1baed3b3acc766b7483a53a
Gordon Soutter <gordon.soutter@outlook.com>
2020-06-21 11:28:51 +0100

    adds repository, viewmodel and Recycler view, to display questions list

--- app/src/main/java/io/gogz/qperdiem/MainActivity.java
+++ app/src/main/java/io/gogz/qperdiem/MainActivity.java
@@ -7,11 +7,15 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+
+import io.gogz.qperdiem.room_db.QuestionListAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,8 +23,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        final QuestionListAdapter adapter = new QuestionListAdapter(this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
