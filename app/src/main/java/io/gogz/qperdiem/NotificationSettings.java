23c540645b7264b5a66782adf22e6d3fa117c11c
Gordon Soutter <gordon.soutter@outlook.com>
2020-07-15 12:51:05 +0100

    adds nofitications setting, saving on off setting to sharepreferences

--- app/src/main/java/io/gogz/qperdiem/NotificationSettings.java
+++ app/src/main/java/io/gogz/qperdiem/NotificationSettings.java
@@ -1,9 +1,11 @@
 package io.gogz.qperdiem;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,10 +18,27 @@ public class NotificationSettings extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
 
+        SharedPreferences sharedPrefs = getApplicationContext().getSharedPreferences("QperDiemPrefs", MODE_PRIVATE); // 0 - for private mode
+        SharedPreferences.Editor sharedPrefsEditor = sharedPrefs.edit();
+
+        boolean contextOn = sharedPrefs.getBoolean("contextNotificationOn", false);
+
+
+        final ToggleButton toggleButtonNotificationOnOff = findViewById(R.id.toggleButtonNotificationOnOff);
+
+        toggleButtonNotificationOnOff.setChecked(contextOn);
+
 
         final Button button = findViewById(R.id.button_save);
+
         button.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view) {
+
+
+                sharedPrefsEditor.putBoolean("contextNotificationOn", toggleButtonNotificationOnOff.isChecked());
+                sharedPrefsEditor.apply();
+
+
                 Toast.makeText(
                         NotificationSettings.this,
                         R.string.settings_saved,
